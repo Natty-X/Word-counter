e8aea551df9a2134021556cc93d4fafe713b6d73
Nathaniel Parah <nathanielparah579@gmail.com>
2026-01-09 11:58:12 +0100

    Second commit with initial implentation of program
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::io;
 
-struct PromptLength {
-    word_count: usize,
-    lines_count: usize,
-    character_count: usize,
+struct PromptLength<'a> {
+    word_count: &'a String,
+    lines_count: &'a String,
+    character_count: &'a String,
 }
 
-impl PromptLength {
+impl<'a> PromptLength<'a> {
  
     fn read_input() -> String {
         let mut input = String::new();
@@ -18,14 +18,34 @@ impl PromptLength {
         input
     }   
 
-    fn word_count(&self, mut input: Vec<String>) -> usize {
-        let prompt = Self::read_input();
-        input.push(prompt);
-        input.len()
+    fn word_count(&self) -> usize {
+        self.word_count.split_whitespace().count()
+    }
+
+    fn lines_count(&self) -> usize{
+        self.lines_count.lines().count()
+    }
+
+    fn character_count(&self) -> usize {
+        self.character_count.chars().count()
     }
 }
 
 
 fn main() {
-    println!("Hello, world!");
+    let input = PromptLength::read_input();
+    
+    let prompt = PromptLength {
+        word_count: &input,
+        lines_count: &input,
+        character_count: &input,
+    };
+
+    let word_count = prompt.word_count();
+    
+    let lines_count = prompt.lines_count();
+
+    let character_count = prompt.character_count();
+
+    println!("\n\n\nWord count>> {:?},\n\nlines count>> {:?},\n\ncharacters count>> {:?}", word_count, lines_count, character_count);
 }
